d3b3cdb0ebbd71334e0179f26c8ecf15674c0fd0
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-05-04 09:16:03 -0400

    Add files via upload

---
+++ example_mods/Vs Nonsense Psych Port/stages/whiteVoidNH.lua
@@ -0,0 +1,8 @@
+-- Auto-converted from V-Slice stage whiteVoidNH.json
+function onCreate()
+    makeLuaSprite("blacksquare", nil, -500, -400)
+    makeGraphic("blacksquare", 2600, 1380, "#FFFFFF")
+    setScrollFactor("blacksquare", 0, 0)
+    addLuaSprite("blacksquare", false)
+    setProperty('defaultCamZoom', 0.7)
+end
